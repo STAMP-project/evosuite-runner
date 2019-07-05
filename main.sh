3677a111497a6cb97afaa9b2c1d6698004376309
pderakhshanfar <p.derakhshanfar@tudelft.nl>
2019-07-05 13:41:27 +0200

    Fix issue #6

--- main.sh
+++ main.sh
@@ -1,9 +1,12 @@
 flagtest=0
 flagmodel=0
-clone_seed=(0.3 0.5 0.8 1.0)
+
 
 # params="-generateTests -Dalgorithm=DynaMOSA -Dpopulation=100 -Dsearch_budget=180"
 # params="-generateTests -Dalgorithm=DynaMOSA"
+
+# ToDo: set the value of clone_seed,
+clone_seed=(0.3 0.5 0.8 1.0)
 search_budget=180
 population=100
 while [ "$1" != "" ]; do
@@ -35,7 +38,7 @@ do
       if [ "$flagmodel" -eq 1 ]; then
         echo "Model seeding is on - seed_clone=$clone_seed_p"
         model_dir="analysis-result/$project/models"
-        java -d64 -Xmx4000m -jar evosuite-master-1.0.7-SNAPSHOT.jar -generateMOSuite -Dalgorithm=DynaMOSA -Dpopulation=100 -Dsearch_budget=215000 -Dstopping_condition=MAXFITNESSEVALUATIONS -Dshow_progress=FALSE -projectCP "$projectCP" -class "$class" -Dseed_clone="$clone_seed_p" -Donline_model_seeding=TRUE -Dmodel_path="$model_dir" > "logs/model_seeding/$project-$class-$clone_seed_p-$i-out.txt" 2> "logs/model_seeding/$project-$class-$clone_seed_p-$i-err.txt" &
+        java -d64 -Xmx4000m -jar evosuite-master-1.0.7-SNAPSHOT.jar -generateMOSuite -Dalgorithm=DynaMOSA -Dpopulation=100 -Dsearch_budget=215000 -Dstopping_condition=MAXFITNESSEVALUATIONS -Dshow_progress=FALSE -projectCP "$projectCP" -class "$class" -Dseed_clone="$clone_seed_p" -Donline_model_seeding=TRUE -Dmodel_path="$model_dir" -Dtest_dir="generated_tests/model_seeding/$project-$class-$clone_seed_p-$i" > "logs/model_seeding/$project-$class-$clone_seed_p-$i-out.txt" 2> "logs/model_seeding/$project-$class-$clone_seed_p-$i-err.txt" &
         pid=$!
         . parsing.sh "model" $pid $i $project $class $clone_seed_p &
       elif [[ "$flagtest" -eq 1  ]]; then
@@ -44,14 +47,14 @@ do
           echo "There is no test for class $class. Test seeding is skipped"
        else
          echo "Test seeding is on - seed_clone=$clone_seed_p"
-         java -d64 -Xmx4000m -jar evosuite-master-1.0.7-SNAPSHOT.jar -generateMOSuite -Dalgorithm=DynaMOSA -Dpopulation=100 -Dsearch_budget=215000 -Dstopping_condition=MAXFITNESSEVALUATIONS -projectCP "$projectCP" -class "$class" -Dseed_clone="$clone_seed_p" -Dcarve_object_pool=TRUE -Dselected_junit="$junits" > "logs/test_seeding/$project-$class-$clone_seed_p-$i-out.txt" 2> "logs/test_seeding/$project-$class-$clone_seed_p-$i-err.txt" &
+         java -d64 -Xmx4000m -jar evosuite-master-1.0.7-SNAPSHOT.jar -generateMOSuite -Dalgorithm=DynaMOSA -Dpopulation=100 -Dsearch_budget=215000 -Dstopping_condition=MAXFITNESSEVALUATIONS -projectCP "$projectCP" -class "$class" -Dseed_clone="$clone_seed_p" -Dcarve_object_pool=TRUE -Dselected_junit="$junits" -Dtest_dir="generated_tests/test_seeding/$project-$class-$clone_seed_p-$i" > "logs/test_seeding/$project-$class-$clone_seed_p-$i-out.txt" 2> "logs/test_seeding/$project-$class-$clone_seed_p-$i-err.txt" &
          pid=$!
          . parsing.sh "test" $pid $i $project $class $clone_seed_p &
         fi
       else
         echo "No seeding is on"
         echo "$projectCP"
-        java -d64 -Xmx4000m -jar evosuite-master-1.0.7-SNAPSHOT.jar -generateMOSuite -Dalgorithm=DynaMOSA -Dpopulation=100 -Dsearch_budget=215000 -Dstopping_condition=MAXFITNESSEVALUATIONS -projectCP "$projectCP" -class "$class" > "logs/no_seeding/$project-$class-$i-out.txt" 2> "logs/no_seeding/$project-$class-$i-err.txt" &
+        java -d64 -Xmx4000m -jar evosuite-master-1.0.7-SNAPSHOT.jar -generateMOSuite -Dalgorithm=DynaMOSA -Dpopulation=100 -Dsearch_budget=215000 -Dstopping_condition=MAXFITNESSEVALUATIONS -projectCP "$projectCP" -class "$class" -Dtest_dir="generated_tests/no_seeding/$project-$class-$clone_seed_p-$i" > "logs/no_seeding/$project-$class-$i-out.txt" 2> "logs/no_seeding/$project-$class-$i-err.txt" &
         pid=$!
         . parsing.sh "no" $pid $i $project $class &
         fi
