c3bf76d74b43e0e8da45f2553d4e3c74f4f2240d
Mohamed Boussaa <mohamedboussaa3@gmail.com>
2019-10-22 16:18:32 +0200

    clean model generator script

--- modelGenerator.sh
+++ modelGenerator.sh
@@ -4,10 +4,7 @@ IFS=',' read -r -a array <<< "$ligne"
 
 package="${array[0]}"
 project="${array[1]}"
-echo $ligne
-echo $project
-echo $package
-echo 'momo'
+
 echo 'java -d64 -jar botsing-model-generation-1.0.7.jar -project_cp '"bins/$project"' -project_prefix '"$package"' -out_dir '"analysis-result/$project"''
 
 java -d64 -jar botsing-model-generation-1.0.7.jar -project_cp "bins/$project" -project_prefix "$package" -out_dir "analysis-result/$project"
