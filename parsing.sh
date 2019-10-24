3334225dc06b9c76849e4f14a0293b307cacb397
pderakhshanfar <p.derakhshanfar@tudelft.nl>
2019-10-24 13:49:20 +0200

    Re-run unsuccessful EvoSuite executions

--- parsing.sh
+++ parsing.sh
@@ -1,11 +1,23 @@
 type=$1
-pid=$2
-execution_idx=$3
-application=$4
-class=$5
-
+population=$2
+search_budget=$3
+pid=$4
+execution_idx=$5
+project=$6
+class=$7
+flagmodel=0
+flagtest=0
 if [ "$type" != "no" ]; then
-seed_clone=$6
+  clone_seed_p=$8
+  resultDir=$9
+  if [ "$type" != "model" ]; then
+    flagmodel=1
+  else
+    flagtest=1
+  fi
+else
+  clone_seed_p=0
+  resultDir=$8
 fi
 
 # Wait until the process with the passed pid ends.
@@ -13,10 +25,18 @@ while kill -0 "$pid"; do
             sleep 1
 done
 
-if [ "$type" != "no" ]; then
-  # Parsing test_seeding & model_seeding executions
-  python python/write_on_csv_file.py $type $execution_idx $application $class $seed_clone &
+if [ -d "$resultDir" ]; then
+  # Parsing the final results
+  if [ "$type" != "no" ]; then
+    # Parsing test_seeding & model_seeding executions
+    python python/write_on_csv_file.py $type $execution_idx $project $class $clone_seed_p &
+  else
+    # Parsing no_seeding executions
+    python python/write_on_csv_file.py $type $execution_idx $project $class &
+  fi
 else
-  # Parsing no_seeding executions
-  python python/write_on_csv_file.py $type $execution_idx $application $class &
+  # Re-run the EvoSuite process
+  . run_evosuite.sh $project $flagmodel $flagtest $clone_seed_p $class $execution_idx $population $search_budget
 fi
+
+
---
+++ run_evosuite.sh
@@ -0,0 +1,45 @@
+project=$1
+flagmodel=$2
+flagtest=$3
+clone_seed_p=$4
+class=$5
+i=$6
+
+population=$7
+search_budget=$8
+
+projectCP=$(python python/generateProjectCP.py "bins/$project")
+      if [ "$flagmodel" -eq 1 ]; then
+        # model_seeding execution mode
+        echo "Model seeding is on - seed_clone=$clone_seed_p"
+        model_dir="analysis-result/$project/models"
+        generatedTestDir="generated_tests/model_seeding/$project-$class-$clone_seed_p-$i"
+        java -d64 -Xmx4000m -jar evosuite-master-1.0.7-SNAPSHOT.jar -generateMOSuite -Dalgorithm=DynaMOSA -Dpopulation=$population -Dsearch_budget=$search_budget  -Dshow_progress=FALSE -projectCP "$projectCP" -class "$class" -Dseed_clone="$clone_seed_p" -Donline_model_seeding=TRUE -Dmodel_path="$model_dir" -Dtest_dir="generated_tests/model_seeding/$project-$class-$clone_seed_p-$i" > "logs/model_seeding/$project-$class-$clone_seed_p-$i-out.txt" 2> "logs/model_seeding/$project-$class-$clone_seed_p-$i-err.txt" &
+        pid=$!
+        . parsing.sh "model" $population $search_budget $pid $i $project $class $clone_seed_p $generatedTestDir &
+        # Parse the execution log and save the useful information in to the model_seeding csv file
+      elif [[ "$flagtest" -eq 1  ]]; then
+        # test_seeding execution mode
+        junits=$(python python/collect-junits.py $project $class)
+        # Check if we have relative tests for the target class or not.
+        if [[ -z "$junits" ]]; then
+          echo "There is no test for class $class. Test seeding is skipped"
+          # If we donot have one, we will skip test_seeding because test_seeding without test is equivalent to no_seeding.
+       else
+         echo "Test seeding is on - seed_clone=$clone_seed_p"
+         generatedTestDir="generated_tests/test_seeding/$project-$class-$clone_seed_p-$i"
+         java -d64 -Xmx4000m -jar evosuite-master-1.0.7-SNAPSHOT.jar -generateMOSuite -Dalgorithm=DynaMOSA -Dpopulation=$population -Dsearch_budget=$search_budget -Dshow_progress=FALSE  -projectCP "$projectCP" -class "$class" -Dseed_clone="$clone_seed_p" -Dcarve_object_pool=TRUE -Dselected_junit="$junits" -Dtest_dir="generated_tests/test_seeding/$project-$class-$clone_seed_p-$i" > "logs/test_seeding/$project-$class-$clone_seed_p-$i-out.txt" 2> "logs/test_seeding/$project-$class-$clone_seed_p-$i-err.txt" &
+         pid=$!
+         . parsing.sh "test" $population $search_budget $pid $i $project $class $clone_seed_p $generatedTestDir &
+         # Parse the execution log and save the useful information in to the test_seeding csv file
+        fi
+      else
+        # no_seeding execution mode
+        echo "No seeding is on"
+        echo "$projectCP"
+        generatedTestDir="generated_tests/no_seeding/$project-$class-$clone_seed_p-$i"
+        java -d64 -Xmx4000m -jar evosuite-master-1.0.7-SNAPSHOT.jar -generateMOSuite -Dalgorithm=DynaMOSA -Dpopulation=$population -Dsearch_budget=$search_budget -Dshow_progress=FALSE  -projectCP "$projectCP" -class "$class" -Dtest_dir="generated_tests/no_seeding/$project-$class-$clone_seed_p-$i" > "logs/no_seeding/$project-$class-$i-out.txt" 2> "logs/no_seeding/$project-$class-$i-err.txt" &
+        pid=$!
+        . parsing.sh "no" $population $search_budget $pid $i $project $class $generatedTestDir &
+        # Parse the execution log and save the useful information in to the no_seeding csv file
+      fi
